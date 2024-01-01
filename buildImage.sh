36aa272da08159a6afd9b6f7fd365f4b08ab6d4f
Morten Harding <83698635+MortenHarding@users.noreply.github.com>
2024-01-01 11:45:30 +0100

    Update buildImage.sh

--- buildImage.sh
+++ buildImage.sh
@@ -1,6 +1,5 @@
 docker container prune -f
 docker image prune -f
 docker build -t mvs:38j-tk5upd2-console .
-# docker run -it --name mvs38j -p 3270:3270/tcp -p 8080:8080/tcp mhardingdk/mvs:38j-tk5upd2-console
-# docker container start -it mvs38j
-# docker exec -it mvs38j c3270 localhost:3270
+# docker run -it --name myMVS -p 3270:3270/tcp -p 8080:8080/tcp mhardingdk/mvs
+# docker exec -it myMVS c3270 localhost:3270
