2c1d54b3bbcff6006cc16850da04f1e38b3c85ae
MortenHarding <morten.harding@icloud.com>
2024-01-07 10:24:54 +0100

    Several changes to compile hercules in a container

--- buildHercules.sh
+++ buildHercules.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd /app/hercules-helper
 git pull
-./hercules-buildall.sh --auto --macports --flavor=sdl-hyperion --no-bashrc --no-envscript --config=herc4.conf
-zip -r herc4x.zip ./herc4x
+cd /app/hercules
+/app/hercules-helper//hercules-buildall.sh --auto --flavor=sdl-hyperion --config=/app/herc4.conf
+. /herc4x/hyperion-init-bash.sh
+cd /app
+rm -rf hercules-helper
+zip -r hercules.zip ./hercules
 $SHELL
